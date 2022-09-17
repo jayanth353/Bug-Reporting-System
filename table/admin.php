bc6fcd82638a78344ecacb820661616d99691ba1
Jayanth 777 <jayanthnalluri353@gmail.com>
2022-09-17 22:16:18 +0530

    add table

--- table/admin.php
+++ table/admin.php
@@ -4,23 +4,7 @@
 	<title>Vuln table</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="logo1.jpeg"/>
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/perfect-scrollbar/perfect-scrollbar.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="css/util.css">
-	<link rel="stylesheet" type="text/css" href="css/main.css">
-<!--===============================================================================================-->
-
+	<link rel="stylesheet" href="table.css">
 </head>
 <body>
 	
@@ -43,8 +27,8 @@
 						</thead>
 						<tbody>
 						<?php
-                               $connection = mysqli_connect("localhost","root","");
-                              $db= mysqli_select_db($connection,"signup");
+                               $connection = mysqli_connect("localhost","id19578455_bugrep","BugReporting$7733");
+							   $db= mysqli_select_db($connection,"id19578455_signup");
 								$selectquery = "select * from report";
 
 								$query=mysqli_query($connection,$selectquery);
@@ -72,19 +56,6 @@
 			</div>
 		</div>
 	</div>
-
-
-	
-
-<!--===============================================================================================-->	
-	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
-<!--===============================================================================================-->
-	<script src="vendor/bootstrap/js/popper.js"></script>
-	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-<!--===============================================================================================-->
-	<script src="vendor/select2/select2.min.js"></script>
-<!--===============================================================================================-->
-	<script src="js/main.js"></script>
 
 </body>
 </html>
